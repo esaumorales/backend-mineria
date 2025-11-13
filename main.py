57e16d7681e22f6d404288a932c1ef1bfdc83b15
esaumorales <esaumorales2322@gmail.com>
2025-11-13 02:16:32 -0500

    push

---
+++ main.py
@@ -0,0 +1,24 @@
+# main.py
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+import numpy as np
+
+app = FastAPI()
+
+# ===== Cargar modelo =====
+with open("modelmodelo_xgb_price.pkl", "rb") as f:
+    model = pickle.load(f)
+
+
+# ===== Definir el request =====
+class PredictRequest(BaseModel):
+    features: list  # ejemplo: [30, 150, 2]
+
+
+# ===== Endpoint =====
+@app.post("/predict")
+def predict(data: PredictRequest):
+    X = np.array([data.features])
+    pred = model.predict(X)
+    return {"prediction": float(pred[0])}
